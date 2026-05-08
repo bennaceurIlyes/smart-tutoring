bbf8c4b94a27c2cc4f6e01bac98a1c0ce7ec878b
Your Name <bennaceurilyes321@gmail.com>
2026-05-08 19:43:44 +0100

    Add legacy URL redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/accounts/:path*',
+        destination: '/login',
+        permanent: true,
+      },
+      {
+        source: '/tutors/list',
+        destination: '/tutors',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
